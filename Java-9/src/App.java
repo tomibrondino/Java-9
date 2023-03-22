5154061466ad06df04977400d1566b90b4ccf301
Tomas Brondino <tomasbrondino2004@gmail.com>
2023-03-21 23:51:58 -0300

    intento jaj
    che me quede acá, a partir del miercoles voy a estar fuera de casa hasta mas o menos el domingo creeria

---
+++ Java-9/src/App.java
@@ -0,0 +1,21 @@
+package ar.edu.utn;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Partido {
+    Scanner teclado=new scanner(System.in)
+    
+    private equipo1 = equipo;
+    private equipo2 = equipo;
+    private golesEquipo1 = int;
+    private golesEquipo2 = int;
+    
+    private static final resultado(equipo)= ResultadoEnum {
+    System.out.println("escriba el nombre del equipo 1");
+    String equipo1=teclado.next.line()
+    Partido equipo1 = new equipo(equipo1);
+
+            this nombre = String
+            this descripcion = String
+    
+    }
